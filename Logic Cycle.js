ec3dc96aa16e4f74222ed5ba141040bc2aa3bf93
AleksCr <sashapolarear@gmail.com>
2017-10-24 13:10:18 +0400

    partial even odds realisation

--- Logic Cycle.js
+++ Logic Cycle.js
@@ -57,7 +57,7 @@ function colorProducion(pos, color, newPos) {
 }
 
 var alg;
-alg = prompt("choose alg in list '|stupidSort|bubbleSort|sheikerSort|' ");//"bubbleSort";
+alg = prompt("choose alg in list '|stupidSort|bubbleSort|sheikerSort|even-odds|' ");//"bubbleSort";
 init_gen();
 arrtest2();
 alert(strtest);
@@ -196,5 +196,40 @@ function visualising(alg) {
                 }, 500);
 
          }
-    }}
+    } else if(alg == "even-odds"){
+        var i = 0;
+        var isEven = 1;
+        var remain = arr.length;
+        var loopsNum = 0;
+        myLoop();
+        function myLoop () {
+            setTimeout(function () {
+                i+=2;
+                if (i < arr.length) {
+                    colorProducion(i-1,"orange",i-1);
+                    colorProducion(i,"orange",i);
+                    if(arr[i-1].rnd > arr[i].rnd){
+                        var tmp = arr[i-1].rnd;
+                        arr[i-1].rnd = arr[i].rnd;
+                        arr[i].rnd = tmp;
+                        colorProducion(i-1,"pink",i-1);
+                        colorProducion(i,"pink",i);
+                        myLoop();
+                    }
+                    else {setTimeout(function () {
+                        colorProducion(i-1,"pink",i-1);
+                        colorProducion(i,"pink",i);
+                    },500);
+                        myLoop();}
+                } else {
+                    remain--;
+                    if(isEven) {i = -1; isEven = 0;} else {i = 0; isEven = 1;}
+                    if(remain<1) loopsNum++;
+                    if(loopsNum>=2) return;
+                    myLoop();
+                }
+            }, 500);
+        }
+    }
+}
 setTimeout(function() {visualising(alg)}, 500);
